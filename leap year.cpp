38e68ed7f4d88ec8679c2519134724bb8e6dfa97
MD.MRIDUL <120597909+mridul17ali@users.noreply.github.com>
2023-04-20 20:37:34 +0600

    Add files via upload

---
+++ leap year.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+ int year;
+ cout<<"Enter any year:"<<endl;
+ cin>>year;
+
+ if( year%4==0 && year%100!=0){
+
+    cout<<"leap year"<<endl;
+ }
+
+else if( year%400==0){
+
+    cout<<"leap year"<<endl;
+ }
+ else
+    cout<<"Not leap year"<<endl;
+
+
+return 0;
+}
