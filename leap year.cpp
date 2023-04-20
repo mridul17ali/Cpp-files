da33a5e4cf876b737d168ed3142c73de416eeea7
MD.MRIDUL <120597909+mridul17ali@users.noreply.github.com>
2023-04-20 20:46:35 +0600

    Update leap year.cpp

--- leap year.cpp
+++ leap year.cpp
@@ -1,24 +1,23 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-int main(){
-
+int main()
+{
  int year;
  cout<<"Enter any year:"<<endl;
  cin>>year;
 
- if( year%4==0 && year%100!=0){
-
-    cout<<"leap year"<<endl;
+ if( year%4==0 && year%100!=0)
+ {
+ cout<<"leap year"<<endl;
  }
 
-else if( year%400==0){
-
-    cout<<"leap year"<<endl;
- }
+else if( year%400==0)
+{
+ cout<<"leap year"<<endl;
+}
  else
     cout<<"Not leap year"<<endl;
-
 
 return 0;
 }
